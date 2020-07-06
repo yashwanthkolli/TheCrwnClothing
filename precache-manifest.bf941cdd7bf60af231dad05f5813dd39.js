c5608532680c14c2a74104f46371bf99d6ad1010
Yashwanth Kolli <yashwanthkolli3271gmail.com>
2020-07-06 09:14:45 +0530

    Updates

---
+++ precache-manifest.bf941cdd7bf60af231dad05f5813dd39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb45acfc7c2664ba404a49766a2d8f6",
+    "url": "/PerfectN008/TheCrwnClthing/index.html"
+  },
+  {
+    "revision": "3a7f2a4092f91f8c1b13",
+    "url": "/PerfectN008/TheCrwnClthing/static/css/main.6510c436.chunk.css"
+  },
+  {
+    "revision": "ed7ed8bae870de05eaaf",
+    "url": "/PerfectN008/TheCrwnClthing/static/js/2.e0181e8d.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/PerfectN008/TheCrwnClthing/static/js/2.e0181e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7f2a4092f91f8c1b13",
+    "url": "/PerfectN008/TheCrwnClthing/static/js/main.473ded49.chunk.js"
+  },
+  {
+    "revision": "3381add7a5ff1708f75a",
+    "url": "/PerfectN008/TheCrwnClthing/static/js/runtime-main.12180fb4.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/PerfectN008/TheCrwnClthing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/PerfectN008/TheCrwnClthing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
